ba28b79d6bdace76e7c0a793c3586040e724bba9
Kostya20292 <78802135+Kostya20292@users.noreply.github.com>
2022-06-21 11:28:20 +0300

    feat: Подключен слайдер swiper к проекту

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.slider-button--next',
+        prevEl: '.slider-button--prev',
+    },
+});
