5d06ec50218f3012a38bd285d9704de99105bbf5
Kostya20292 <78802135+Kostya20292@users.noreply.github.com>
2022-07-02 01:20:14 +0300

    feat: Сверстано модальное окно

--- js/main.js
+++ js/main.js
@@ -33,9 +33,40 @@ const reviewsSlider = new Swiper('.reviews-slider', {
 $('.newsletter').parallax({ imageSrc: '../img/newsletter-bg.jpg' });
 
 const menuButton = document.querySelector('.menu-button');
+const modalButton = document.querySelectorAll('.modal__button--active');
+const modalOverlay = document.querySelector('.modal__overlay');
+const modalDialog = document.querySelector('.modal__dialog');
+const modalClose = document.querySelector('.modal__close');
 
 menuButton.addEventListener('click', () => {
     document
         .querySelector('.navbar-bottom')
         .classList.toggle('navbar-bottom--visible');
+});
+
+modalButton.forEach((modal) => {
+    modal.addEventListener('click', () => {
+        modalOverlay.classList.add('modal__overlay--visible');
+        modalDialog.classList.add('modal__dialog--visible');
+    });
+});
+
+modalClose.addEventListener('click', () => {
+    modalOverlay.classList.remove('modal__overlay--visible');
+    modalDialog.classList.remove('modal__dialog--visible');
+});
+
+modalClose.addEventListener('keypress', (e) => {
+    console.log(e.key);
+    if (e.key === 'Escape') {
+        modalOverlay.classList.remove('modal__overlay--visible');
+        modalDialog.classList.remove('modal__dialog--visible');
+    }
+});
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        modalOverlay.classList.remove('modal__overlay--visible');
+        modalDialog.classList.remove('modal__dialog--visible');
+    }
 });
